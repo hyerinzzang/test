3b525446c660c0448af06ec6a497ec4cecfe7589
김혜린 <@DESKTOP-SKFN5AL>
2020-04-24 14:13:31 +0900

    짅닞닞닞진짜추가

--- gitPrac/src/gitPrac/Test.java
+++ gitPrac/src/gitPrac/Test.java
@@ -21,6 +21,12 @@ public class Test {
 		System.out.println("또추가");
 		System.out.println("브랜치 dev1가 추가");
 		System.out.println("또추가");
+		System.out.println("또추가");
+		System.out.println("조장이 수정함 dev1는 뻗어진상태");
+		System.out.println("브랜치 dev1가 추가");
+		System.out.println("또추가");
+		System.out.println("브랜치 dev1가 추가");
+		System.out.println("또추가");
 		
 		
 
